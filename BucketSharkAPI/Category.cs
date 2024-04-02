9c635bcbaffa0245015d2d187ab97ac3a4203774
Carlos Rodriguez <rodrigcp22@gmail.com>
2024-04-02 22:12:04 +1100

    Add project files.

---
+++ BucketSharkAPI/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace BucketSharkAPI
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string User { get; set; }
+        public string Name { get; set; }
+        public bool Private { get; set; }
+        public decimal Budgeted { get; set; }
+        public decimal Spent { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ BucketSharkAPI/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BucketSharkAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        // Existing GET method...
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from API");
+        }
+
+        // New POST method
+        [HttpPost]
+        public IActionResult Post([FromBody] Category data)
+        {
+            // Process data...
+            return Ok($"Received: {data.Name}");
+        }
+    }
+}
